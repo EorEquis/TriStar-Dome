c5d17d8c25e645545818bff310f0e5058803897c
EorEquis <eeyore@stuffupthere.com>
2020-06-09 08:53:42 -0500

    Initial cleanup

--- Arduino/Observatory_Firmware/Observatory_Firmware.ino
+++ Arduino/Observatory_Firmware/Observatory_Firmware.ino
@@ -14,162 +14,165 @@
  *  1.0.1       2017DEC06   EOR   Clean up some unnecessary code to set motor limits that can be written to SMC's NVRAM
  *  1.1.0       2017DEC07   EOR   Remove LCD stuff.  It's dumb.
  *  1.1.1       2018FEB09   EOR   Slow the roof down, re-order status checks to not return error on connect
+ *  2.0.0       2020JUN09   EOR   Sparta Obs Version, cleanup, add pushbutton
  **************************************************************************/
  
 #include <SoftwareSerial.h>
 
 // Pololu SMC config
-const int rxPin = 3;          // pin 3 connects to SMC TX   : Green
-const int txPin = 4;          // pin 4 connects to SMC RX   : Orange
-const int resetPin = 5;       // pin 5 connects to SMC nRST : Brown
-const int errPin = 6;         // pin 6 connects to SMC ERR  : Blue
+  const int rxPin = 3;          // pin 3 connects to SMC TX   : Green
+  const int txPin = 4;          // pin 4 connects to SMC RX   : Orange
+  const int resetPin = 5;       // pin 5 connects to SMC nRST : Brown
+  const int errPin = 6;         // pin 6 connects to SMC ERR  : Blue
+
+// Other Pins
+  const int buttonPin = 7;      // pin 7 connects to button   : Yellow
 
 // SMC Variable IDs
-#define ERROR_STATUS 0
-#define LIMIT_STATUS 3
-#define TARGET_SPEED 20
-#define SPEED 21
+  #define ERROR_STATUS 0
+  #define LIMIT_STATUS 3
+  #define TARGET_SPEED 20
+  #define SPEED 21
 
 
 // SMC motor limit IDs
-
-#define DECELERATION 2
+  #define DECELERATION 2
 
 // Flags and counters
-bool debugFlag = 0;
-unsigned long lastMillis = 0;
-unsigned long currentMillis = 0;
+  bool debugFlag = 0;
+  unsigned long lastMillis = 0;
+  unsigned long currentMillis = 0;
 
 // ASCOM ShutterState Enumeration : http://www.ascom-standards.org/Help/Developer/html/T_ASCOM_DeviceInterface_ShutterState.htm
-#define shutterOpen 0
-#define shutterClosed 1
-#define shutterOpening 2
-#define shutterClosing 3
-#define shutterError 4
+  #define shutterOpen 0
+  #define shutterClosed 1
+  #define shutterOpening 2
+  #define shutterClosing 3
+  #define shutterError 4
 
 // Other variables
-String strCmd;
-unsigned int limitStatus = 0;
-unsigned int errorStatus = 0;
-int currentSpeed = 0;
-int targetSpeed = 0;
-int shutterState = 1;
+  String strCmd;
+  unsigned int limitStatus = 0;
+  unsigned int errorStatus = 0;
+  int currentSpeed = 0;
+  int targetSpeed = 0;
+  int shutterState = 1;
 
 // SoftwareSerial for communication w/ SMC
-SoftwareSerial smcSerial = SoftwareSerial(rxPin, txPin);
+  SoftwareSerial smcSerial = SoftwareSerial(rxPin, txPin);
 
 
 //***** Main Setup() and Loop() functions *****//
 
 void setup() {
+
   // Serial lines
-  Serial.begin(9600);       // For serial monitor troubleshooting
-  smcSerial.begin(19200);   // Begin serial to SMC
+    Serial.begin(9600);       // For serial monitor troubleshooting
+    smcSerial.begin(19200);   // Begin serial to SMC
 
   // Reset SMC when Arduino starts up
-  resetSMC();
+    resetSMC();
   
   // Set the startup values
-  shutterState=getInfo();
+    shutterState=getInfo();
   
   // Begin tracking elapsed time for getInfo()
-  currentMillis=millis();
-  lastMillis=currentMillis;  
+    currentMillis=millis();
+    lastMillis=currentMillis;  
+
 }   // end setup()
 
 void loop() {
 
   // Refresh info packet every 1s
-  currentMillis=millis();
-  if (currentMillis - lastMillis > 500)
-  {
-    lastMillis=currentMillis;
-    shutterState=getInfo();  
-    if (debugFlag)
+    currentMillis=millis();
+    if (currentMillis - lastMillis > 500)
       {
-        Serial.print("getInfo() called, shutterState is ");
-        Serial.println(shutterState);
-        Serial.print("limitStatus : ");
-        Serial.println(limitStatus);
-        Serial.print("errorStatus : ");
-        Serial.println(errorStatus);
-        Serial.print("currentSpeed : ");
-        Serial.println(currentSpeed);
-      }
-  }
+        lastMillis=currentMillis;
+        shutterState=getInfo();  
+        if (debugFlag)
+          {
+            Serial.print("getInfo() called, shutterState is ");
+            Serial.println(shutterState);
+            Serial.print("limitStatus : ");
+            Serial.println(limitStatus);
+            Serial.print("errorStatus : ");
+            Serial.println(errorStatus);
+            Serial.print("currentSpeed : ");
+            Serial.println(currentSpeed);
+          }   // end if debug
+      }   // end if millis
 
   while (Serial.available() > 0)
-  {
-    strCmd = Serial.readStringUntil('#');
-    /*******************************************************
-     * Supported commands
-     * abrt     :       Abort any/all current movement (Stop the roof NOW)
-     * clos     :       Close Roof (Shutter)
-     * open     :       Open Roof (Shutter)
-     * info     :       Request from ASCOM for ShutterStatus
-     * reset    :       Reset the SMC.  This is not an ASCOM method, but included for troubleshooting/testing
-    *******************************************************/
-    if (strCmd == "abrt")
     {
-      setMotorLimit(DECELERATION, 0);
-      setMotorSpeed(0);
-      setMotorLimit(DECELERATION, 3);
-    }
-
-    else if (strCmd == "open")
-    {
-      if (shutterState == shutterClosed)
-      {
-        //exitSafeStart();
-        setMotorSpeed(1400);
-        Serial.print(shutterOpening);
-        Serial.println("#");
-      }
-      else
-      {
-        Serial.print(shutterError);
-        Serial.println("#");
-      }
-    }
+      strCmd = Serial.readStringUntil('#');
+      /*******************************************************
+       * Supported commands
+       * abrt     :       Abort any/all current movement (Stop the roof NOW)
+       * clos     :       Close Roof (Shutter)
+       * open     :       Open Roof (Shutter)
+       * info     :       Request from ASCOM for ShutterStatus
+       * reset    :       Reset the SMC.  This is not an ASCOM method, but included for troubleshooting/testing
+      *******************************************************/
+      if (strCmd == "abrt")
+        {
+          setMotorLimit(DECELERATION, 0);
+          setMotorSpeed(0);
+          setMotorLimit(DECELERATION, 3);
+        }   
+  
+      else if (strCmd == "open")
+        {
+          if (shutterState == shutterClosed)
+          {
+            //exitSafeStart();
+            setMotorSpeed(1400);
+            Serial.print(shutterOpening);
+            Serial.println("#");
+          }
+          else
+          {
+            Serial.print(shutterError);
+            Serial.println("#");
+          }
+        }
 
     else if (strCmd == "clos")
-    {
-      if (shutterState == shutterOpen)
       {
-        //exitSafeStart();
-        setMotorSpeed(-1400);
-        Serial.print(shutterClosing);
-        Serial.println("#");
-
-      
+        if (shutterState == shutterOpen)
+          {
+            //exitSafeStart();
+            setMotorSpeed(-1400);
+            Serial.print(shutterClosing);
+            Serial.println("#");
+          }
+        else
+          {
+            Serial.print(shutterError);
+            Serial.println("#");
+          }      
       }
-      else
-      {
-        Serial.print(shutterError);
-        Serial.println("#");
-      }      
-    }
 
     else if (strCmd == "info")
-    {
-      if (debugFlag)
       {
-        Serial.print("limitStatus : ");
-        Serial.println(limitStatus);
-        Serial.print("errorStatus : ");
-        Serial.println(errorStatus);
-        Serial.print("currentSpeed : ");
-        Serial.println(currentSpeed);
+        if (debugFlag)
+          {
+            Serial.print("limitStatus : ");
+            Serial.println(limitStatus);
+            Serial.print("errorStatus : ");
+            Serial.println(errorStatus);
+            Serial.print("currentSpeed : ");
+            Serial.println(currentSpeed);
+          }
+        Serial.print(shutterState);
+        Serial.println("#");
       }
-      Serial.print(shutterState);
-      Serial.println("#");
-    }
 
     else if (strCmd == "reset")
-    {
-      resetSMC();
-      //exitSafeStart();
-    }
+      {
+        resetSMC();
+        //exitSafeStart();
+      }
   }
 } // End loop()
 
@@ -193,85 +196,85 @@ int getInfo()  //TODO Write This
   }
   */
   if (currentSpeed == 0 && limitStatus == 128)
-  {
-    return shutterOpen;
-  }
+    {
+      return shutterOpen;
+    }
 
   else if (currentSpeed == 0 && limitStatus == 256)
-  {
-      return shutterClosed;
-  }
+    {
+        return shutterClosed;
+    }
 
   else if (currentSpeed > 0 && (limitStatus == 0 || limitStatus == 144))
-  {
-    return shutterOpening;
-  }
+    {
+      return shutterOpening;
+    }
   
   else if ((currentSpeed < 0 && (limitStatus == 0) || limitStatus == 272))
-  {
-    return shutterClosing;
-  }
+    {
+      return shutterClosing;
+    }
+
   else if (errorStatus != 0)
-  {
-    return shutterError;
-  }
+    {
+      return shutterError;
+    }
 } //end getInfo()
 
 // read an SMC serial byte
 int readSMCByte()
-{
-  char c;
-  if(smcSerial.readBytes(&c, 1) == 0){ return -1; }
-  return (byte)c;
-}
+  {
+    char c;
+    if(smcSerial.readBytes(&c, 1) == 0){ return -1; }
+    return (byte)c;
+  }
 
 unsigned char setMotorLimit(unsigned char  limitID, unsigned int limitValue)
-{
-  smcSerial.write(0xA2);
-  smcSerial.write(limitID);
-  smcSerial.write(limitValue & 0x7F);
-  smcSerial.write(limitValue >> 7);
-  return readSMCByte();
-}
+  {
+    smcSerial.write(0xA2);
+    smcSerial.write(limitID);
+    smcSerial.write(limitValue & 0x7F);
+    smcSerial.write(limitValue >> 7);
+    return readSMCByte();
+  }
 
 // speed should be a number from -3200 to 3200
 // TODO : I'll be using a set speed, so this can probbaly go away to save mem.
 void setMotorSpeed(int speed)
-{
-  if (speed < 0)
   {
-    smcSerial.write(0x86);  // motor reverse command
-    speed = -speed;  // make speed positive
+    if (speed < 0)
+    {
+      smcSerial.write(0x86);  // motor reverse command
+      speed = -speed;  // make speed positive
+    }
+    else
+    {
+      smcSerial.write(0x85);  // motor forward command
+    }
+    smcSerial.write(speed & 0x1F);
+    smcSerial.write(speed >> 5);
   }
-  else
-  {
-    smcSerial.write(0x85);  // motor forward command
-  }
-  smcSerial.write(speed & 0x1F);
-  smcSerial.write(speed >> 5);
-}
 
 // returns the specified variable as an unsigned integer.
 // if the requested variable is signed, the value returned by this function
 // should be typecast as an int.
 int getSMCVariable(unsigned char variableID)
-{
-  smcSerial.write(0xA1);
-  smcSerial.write(variableID);
-  return readSMCByte() + 256 * readSMCByte();
-}
+  {
+    smcSerial.write(0xA1);
+    smcSerial.write(variableID);
+    return readSMCByte() + 256 * readSMCByte();
+  }
 
 void resetSMC()
-{
-  pinMode(resetPin, OUTPUT);
-  digitalWrite(resetPin, LOW);  // reset SMC
-  delay(1);  // wait 1 ms
-  pinMode(resetPin, INPUT);  // let SMC run again
-
-  // must wait at least 1 ms after reset before transmitting
-  delay(10);
-
-  // Set up motor controller.
-  smcSerial.write(0xAA);  // send baud-indicator byte
-}
-
+  {
+    pinMode(resetPin, OUTPUT);
+    digitalWrite(resetPin, LOW);  // reset SMC
+    delay(1);  // wait 1 ms
+    pinMode(resetPin, INPUT);  // let SMC run again
+  
+    // must wait at least 1 ms after reset before transmitting
+    delay(10);
+  
+    // Set up motor controller.
+    smcSerial.write(0xAA);  // send baud-indicator byte
+  }
